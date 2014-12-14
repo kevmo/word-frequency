abc0a83cf170a4496fd354d6cd0e6ccb7a970148
kevmo <k.james.moore@gmail.com>
2014-12-14 12:18:12 -0600

    Config added

---
+++ config.py
@@ -0,0 +1,25 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = False
+    SECRET_KEY = 'blahblahblah'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
